ae245aaf8df3f480530b1c9088b69d6c721d3a1a
nikunj-gupta <ng2531@nyu.edu>
2025-01-11 14:11:50 -0800

    updated

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,7 +5,11 @@
 # conda activate dmcg
 
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y 
+
+pip install protobuf==3.20.* sacred numpy scipy gym==0.11 matplotlib seaborn \
+    pyyaml pygame pytest probscale imageio snakeviz tensorboard-logger pymongo setproctitle 
 pip install git+https://github.com/oxwhirl/smacv2.git 
+
 pip install torch_geometric
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.3.1+cu121.html # Optional dependencies: 
-pip install pymongo setproctitle sacred pyyaml tensorboard_logger matplotlib 
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.5.1+cu121.html 
+pip install pogema pogema-toolbox 
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 all: 
 	clear 
-	python3 src/main.py --config=dmcg --env-config=gather with cg_edges=full seed=10 
+	CUDA_VISIBLE_DEVICES=7 python3 src/main.py --config=dmcg --env-config=gather with cg_edges=full seed=10 
