c12a1d2c355d4c5abdf13ba8fe4ce7f58d7ee92e
Nikunj-Gupta <ng2531@nyu.edu>
2025-01-11 11:41:20 -0800

    environment setup

---
+++ install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Install PyTorch and Python Packages
+
+# conda create -n dcg python=3.11 -y
+# conda activate dcg
+
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y 
+pip install git+https://github.com/oxwhirl/smacv2.git 
+pip install torch_geometric
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.3.1+cu121.html # Optional dependencies: 
+pip install pymongo setproctitle sacred pyyaml tensorboard_logger matplotlib 
